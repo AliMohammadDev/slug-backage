62bcff3f59afede5426b33fd215e86761fbf98a6
alishadel2001 <alishadel2001@gmail.com>
2025-06-23 02:12:06 +0300

    fix: support Laravel 11"

--- src/SlugServiceProvider.php
+++ src/SlugServiceProvider.php
@@ -12,7 +12,6 @@ class SlugServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../src/Databases/Migrations');
-        $this->loadViewsFrom(__DIR__ . '/../src/views/', 'wdd/slug');
     }
 
     public function register() {}
